7d33a1be62c184e9a166c793b3d89984579742ab
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-02-02 22:28:17 +0530

    Accordion complete

--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p>{isExpanded && info}</p>
+    </>
+  );
 };
 
 export default Question;
