db3f82807abf240a5c2d5ce5d81411ebd4ce8c8d
Abhishek Singh <abhisheksinghxix@gmail.com>
2022-02-02 23:44:13 +0530

    Menu project complete

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import data from './data';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, setMenuItems }) => {
+  return (
+    <section className='btn-container'>
+      {categories.length > 1 && (
+        <button className='filter-btn' onClick={() => setMenuItems([...data])}>
+          All
+        </button>
+      )}
+
+      {categories.map((category) => (
+        <button
+          className='filter-btn'
+          onClick={() => {
+            setMenuItems(data.filter((item) => item.category === category));
+          }}>
+          {category}
+        </button>
+      ))}
+    </section>
+  );
 };
 
 export default Categories;
--- menu/src/Menu.js
+++ menu/src/Menu.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import Categories from './Categories';
+import data from './data';
 
 const Menu = () => {
-  return <h2>menu component</h2>;
+  const [menuItems, setMenuItems] = useState(data);
+  const categories = Array.from(new Set(data.map((item) => item.category)));
+
+  return (
+    <main>
+      <div className='menu section'>
+        <header className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </header>
+        <Categories categories={categories} setMenuItems={setMenuItems} />
+        <section className='section-center'>
+          {menuItems.map((item) => (
+            <article className='menu-item' key={item.id}>
+              <img src={item.img} alt='' className='photo' />
+              <div className='item-info'>
+                <header>
+                  <h4>{item.title}</h4>
+                  <h4 className='price'>{item.price}</h4>
+                </header>
+                <p className='item-text'>{item.desc}</p>
+              </div>
+            </article>
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Menu;
